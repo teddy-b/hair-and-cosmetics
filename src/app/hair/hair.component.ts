0e3ac26ec4df0e69b4dc075a0d56265df42ec9ac
grozdanova <a.grozdanova90@gmail.com>
2017-01-01 14:35:17 +0200

    Add offer model

--- src/app/hair/hair.component.ts
+++ src/app/hair/hair.component.ts
@@ -13,4 +13,5 @@ export class HairComponent implements OnInit {
   ngOnInit() {
   }
 
+
 }
---
+++ src/app/models/offer-model.ts
@@ -0,0 +1,7 @@
+export interface OfferModel {
+    StudioName: string;
+    Image: string;
+    OfferTitle: string;
+    OfferDetails: string;
+    Price: number;
+}
